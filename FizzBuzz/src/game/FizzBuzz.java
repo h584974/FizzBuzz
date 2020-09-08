c0a24ce538a41d9b58e931e05acf7b12574baa44
olive <olive@DESKTOP-R044BGP>
2020-09-09 01:42:10 +0200

    better isDivisibleBy method

--- FizzBuzz/src/game/FizzBuzz.java
+++ FizzBuzz/src/game/FizzBuzz.java
@@ -8,13 +8,13 @@ public class FizzBuzz {
 	
 	public static void playFizzBuzz(int stop) {
 		for(int i = 1; i <= stop; i++) {
-			if(isDivisibleBy(i,3) && isDivisibleBy(i,5)) {
+			if(isDivisibleBy2(i,3) && isDivisibleBy(i,5)) {
 				System.out.println("FIZZ BUZZ!");
 			}
-			else if(isDivisibleBy(i,3)) {
+			else if(isDivisibleBy2(i,3)) {
 				System.out.println("FIZZ!");
 			}
-			else if(isDivisibleBy(i,5)) {
+			else if(isDivisibleBy2(i,5)) {
 				System.out.println("BUZZ!");
 			}
 			else {
@@ -23,13 +23,21 @@ public class FizzBuzz {
 		}
 	}
 	
+	@Deprecated
 	private static boolean isDivisibleBy(int input, int divideBy) {
-			Double result = (double)input / divideBy;
+			double result = (double)input / divideBy;
 			String s = String.valueOf(result);
 			int indexOfDecimal = s.indexOf('.');
 			String finalS = s.substring(indexOfDecimal + 1);
 			
 			return Long.parseLong(finalS) == 0;
 		}
+	
+	private static boolean isDivisibleBy2(int input, int divideBy) {
+		double divised = (double)input / divideBy;
+		int divisedInteger = (int)divised;
+		
+		return divised == divisedInteger;
+	}
 	
 }
