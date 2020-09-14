b9f1f7c2f0fc3ec4d5c90250f05ff776f169c264
olive <olive@DESKTOP-R044BGP>
2020-09-14 21:00:58 +0200

    Version 2.0

--- FizzBuzz/src/game/FizzBuzz.java
+++ FizzBuzz/src/game/FizzBuzz.java
@@ -1,20 +1,23 @@
 package game;
 
+import java.util.stream.*;
+
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		playFizzBuzz(100);
+		playFizzBuzz2(100);
 	}
 	
+	@Deprecated
 	public static void playFizzBuzz(int stop) {
 		for(int i = 1; i <= stop; i++) {
-			if(isDivisibleBy2(i,3) && isDivisibleBy2(i,5)) {
+			if(isDivisible(i,3) && isDivisible(i,5)) {
 				System.out.println("FIZZ BUZZ!");
 			}
-			else if(isDivisibleBy2(i,3)) {
+			else if(isDivisible(i,3)) {
 				System.out.println("FIZZ!");
 			}
-			else if(isDivisibleBy2(i,5)) {
+			else if(isDivisible(i,5)) {
 				System.out.println("BUZZ!");
 			}
 			else {
@@ -23,8 +26,25 @@ public class FizzBuzz {
 		}
 	}
 	
+	public static void playFizzBuzz2(int stop) {
+		IntStream.range(1, stop + 1).forEach(i -> {
+			if(isDivisible2(i,3) && isDivisible2(i,5)) {
+				System.out.println("FIZZ BUZZ!");
+			}
+			else if(isDivisible2(i,3)) {
+				System.out.println("FIZZ!");
+			}
+			else if(isDivisible2(i,5)) {
+				System.out.println("BUZZ!");
+			}
+			else {
+				System.out.println(i);
+			}
+		});
+	}
+	
 	@Deprecated
-	private static boolean isDivisibleBy(int input, int divideBy) {
+	private static boolean isDivisible(int input, int divideBy) {
 			double result = (double)input / divideBy;
 			String s = String.valueOf(result);
 			int indexOfDecimal = s.indexOf('.');
@@ -33,11 +53,8 @@ public class FizzBuzz {
 			return Long.parseLong(finalS) == 0;
 		}
 	
-	private static boolean isDivisibleBy2(int input, int divideBy) {
-		double divised = (double)input / divideBy;
-		int divisedInteger = (int)divised;
-		
-		return divised == divisedInteger;
+	private static boolean isDivisible2(int input, int divideBy) {
+		return (double)input / divideBy == input / divideBy;
 	}
 	
 }
